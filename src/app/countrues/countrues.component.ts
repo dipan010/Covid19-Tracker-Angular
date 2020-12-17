5b5c65519d7fb63b7289833f723090bf168767a0
Dipan Ghosh <dipan.ghosh@hotmail.com>
2020-12-17 23:26:52 +0530

    Countries componebt made

---
+++ src/app/countrues/countrues.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-countrues',
+  templateUrl: './countrues.component.html',
+  styleUrls: ['./countrues.component.css']
+})
+export class CountruesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
