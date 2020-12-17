df4149a44bac3f34f54927695e06f4cf2b8ff9c7
Dipan Ghosh <dipan.ghosh@hotmail.com>
2020-12-17 23:53:31 +0530

    Country routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CountruesComponent } from './countrues/countrues.component';
 //import { HelpfulLinksComponent } from './helpful-links/helpful-links.component';
 //import { FAQComponent } from './faq/faq.component';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +13,8 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   { path: 'home', component: HomeComponent },
-  { path: 'landing', component:LandingComponent }
+  { path: 'landing', component:LandingComponent },
+  { path: 'country', component:CountruesComponent }
   //{ path: 'helpful', component: HelpfulLinksComponent },
   //{ path: 'faq',      component: FAQComponent },
 ];
