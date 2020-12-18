234852272038048bdce4db572aec213a9b1a303e
Dipan Ghosh <dipan.ghosh@hotmail.com>
2020-12-19 01:56:01 +0530

    Shifted all components

---
+++ src/app/components/countrues/countrues.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { country } from '../../country.model';
+import { CoronaService } from '../../shared/corona.service';
+
+@Component({
+  selector: 'app-countrues',
+  templateUrl: './countrues.component.html',
+  styleUrls: ['./countrues.component.css']
+})
+export class CountruesComponent implements OnInit {
+  countries$: country[]
+
+  constructor( private cs: CoronaService) { }
+
+  ngOnInit() {
+    return this.cs.getDataCountryWise()
+    .subscribe(data => this.countries$ = data);
+  }
+
+}
