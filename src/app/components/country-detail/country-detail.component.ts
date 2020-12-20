1f7e2134134616b0d910799d37c06b0d9a6a263b
Dipan Ghosh <dipan.ghosh@hotmail.com>
2020-12-20 23:27:18 +0530

    queryParams completed

--- src/app/components/country-detail/country-detail.component.ts
+++ src/app/components/country-detail/country-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-country-detail',
   templateUrl: './country-detail.component.html',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryDetailComponent implements OnInit {
 
-  constructor() { }
+  country: number;
+  cases: number;
+  active: number;
+  recovered: number;
+  deaths: number;
+  casesPerOneMillion: number;
+  deathsPerOneMillion: number;
+  activePerOneMillion: number;
+  recoveredPerOneMillion: number;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params =>{
+      this.country = params["country"];
+      this.cases = params["cases"];
+      this.active = params["active"];
+      this.recovered = params["recovered"];
+      this.deaths = params["deaths"];
+      this.casesPerOneMillion = params["casesPerOneMillion"];
+      this.deathsPerOneMillion = params["deathsPerOneMillion"];
+      this.activePerOneMillion = params["activePerOneMillion"];
+      this.recoveredPerOneMillion = params["recoveredPerOneMillion"]
+    })
   }
-
 }
