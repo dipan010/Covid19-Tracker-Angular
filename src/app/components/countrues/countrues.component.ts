aacea17c57a0ebf5b73676d8c9b61cdfc4e797fb
Dipan Ghosh <dipan.ghosh@hotmail.com>
2020-12-20 18:07:07 +0530

    Added QueryParams

--- src/app/components/countrues/countrues.component.ts
+++ src/app/components/countrues/countrues.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 import { country } from '../../country.model';
 import { CoronaService } from '../../shared/corona.service';
 
@@ -10,11 +11,28 @@ import { CoronaService } from '../../shared/corona.service';
 export class CountruesComponent implements OnInit {
   countries$: country[]
 
-  constructor( private cs: CoronaService) { }
+  constructor( private cs: CoronaService, private router: Router) { }
 
   ngOnInit() {
     return this.cs.getDataCountryWise()
     .subscribe(data => this.countries$ = data);
+  }
+
+  onSelect(countries){
+    let navigationExtras: NavigationExtras = {
+      queryParams:{
+        country: countries.country,
+        cases: countries.cases,
+        active: countries.active,
+        recovered: countries.recovered,
+        deaths: countries.deaths,
+        casesPerOneMillion: countries.casesPerOneMillion,
+        deathsPerOneMillion: countries.deathsPerOneMillion,
+        activePerOneMillion: countries.activePerOneMillion,
+        recoveredPerOneMillion: countries.recoveredPerOneMillion
+      }
+    }
+    this.router.navigate(["country"], navigationExtras)
   }
 
 }
