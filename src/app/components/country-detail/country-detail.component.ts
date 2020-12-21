43c5879aecd3a6a1517459f41080c6400dce46dd
Dipan Ghosh <dipan.ghosh@hotmail.com>
2020-12-21 18:28:39 +0530

    Routed Params

--- src/app/components/country-detail/country-detail.component.ts
+++ src/app/components/country-detail/country-detail.component.ts
@@ -3,11 +3,12 @@ import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-country-detail',
   templateUrl: './country-detail.component.html',
-  styleUrls: ['./country-detail.component.css']
+  styleUrls: ['./country-detail.component.scss']
 })
 export class CountryDetailComponent implements OnInit {
 
   country: number;
+  population: number;
   cases: number;
   active: number;
   recovered: number;
@@ -16,11 +17,14 @@ export class CountryDetailComponent implements OnInit {
   deathsPerOneMillion: number;
   activePerOneMillion: number;
   recoveredPerOneMillion: number;
+  testsPerOneMillion: number;
+  tests:number;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params =>{
       this.country = params["country"];
+      this.population = params["population"];
       this.cases = params["cases"];
       this.active = params["active"];
       this.recovered = params["recovered"];
@@ -29,6 +33,8 @@ export class CountryDetailComponent implements OnInit {
       this.deathsPerOneMillion = params["deathsPerOneMillion"];
       this.activePerOneMillion = params["activePerOneMillion"];
       this.recoveredPerOneMillion = params["recoveredPerOneMillion"]
+      this.testsPerOneMillion = params["testsPerOneMillion"];
+      this.tests = params["tests"]
     })
   }
 }
