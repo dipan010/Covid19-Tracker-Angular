50c831de2c3eead6940f47a794cc7863c8fa5748
Dipan Ghosh <dipan.ghosh@hotmail.com>
2020-12-21 21:19:36 +0530

    Imported materialui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import '@angular/material'
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   
